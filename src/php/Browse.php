1a6c48ae96773eb3f9bc0350e59bfed5e4d6db71
Jaya Kasa <kasa288@gmail.com>
2015-05-04 14:15:30 -0400

    Updated Docs

--- src/php/Browse.php
+++ src/php/Browse.php
@@ -1,3 +1,4 @@
+<link type="text/css" rel="stylesheet" href="../css/browse_style.css">
 <?php
 /**
  * Created by IntelliJ IDEA.
@@ -7,13 +8,19 @@ session_start();
 include "../html/Header.html";
 
 if(isset($_SESSION['username'])){
-    echo "<script type='text/javascript' src='../js/headermodifiers/searchhead.js'></script>";
-}
-else if(isset($_SESSION['university'])){
-    echo "<script type='text/javascript' src='../js/headermodifiers/altsearchhead.js'></script>";
+    echo "<script type='text/javascript' src='../js/headermodifiers/iteminfohead.js'></script>";
+} else {
+    $_SESSION['itemid'] = $_GET['itemid'];
+    echo "<script type='text/javascript' src='../js/headermodifiers/altiteminfohead.js'></script>";
 }
 //print_r($_SESSION);
+
 ?>
+<div class="headline jumbotron">
+    <div class="container">
+        <h1 align="center">Browse Items Available At Your School</h1>
+    </div>
+</div>
 
 <?php
 include "../html/Footer.html";
